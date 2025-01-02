389780b4183c58ac9ff84256de380d7cee8a562e
EnriqueSazoR <luisrosales9598@gmail.com>
2025-01-02 12:00:41 -0600

    Configuraciones principales (separar proyecto por capas y areas)

---
+++ Libreria.Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Libreria_Biblioteca_.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
